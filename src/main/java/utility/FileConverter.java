1b24f96b813c137de8aad9731e6f3a42ec8bb74c
Thomas David Baker <bakert@gmail.com>
2017-07-16 09:08:42 -0700

    Remove trailing whitespace.

--- src/main/java/utility/FileConverter.java
+++ src/main/java/utility/FileConverter.java
@@ -11,9 +11,9 @@ public class FileConverter {
 	//Convert a file to an array of strings, with each line being an element,
 	public static List<String> readToList(String filename) throws IOException{
 		List<String> toReturn = new ArrayList<>();
-		
+
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
-		
+
 			//BufferedReaders return null at the end of a file, so this cycles through the whole file.
 			for (String line  = br.readLine(); line != null; line = br.readLine()){
 				toReturn.add(line);
@@ -21,12 +21,12 @@ public class FileConverter {
 		}
 		return toReturn;
 	}
-	
+
 	public static List<String> readToList(URL url) throws IOException{
 		List<String> toReturn = new ArrayList<>();
-		
+
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-		
+
 			// BufferedReaders return null at the end of a file, so this cycles through the whole file.
 			for (String line  = br.readLine(); line != null; line = br.readLine()){
 				toReturn.add(line);
@@ -34,14 +34,14 @@ public class FileConverter {
 		}
 		return toReturn;
 	}
-	
+
 	public static int readToInt(String filename) throws IOException {
-		
+
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
 			return Integer.parseInt(br.readLine());
 		}
 	}
-	
+
 	//Write an array of strings to a file, with each element on its own line.
 	public static void writeFile(Collection<String> toWrite, String filename) throws IOException{
 		try(PrintWriter pw = new PrintWriter(filename)) {
@@ -50,7 +50,7 @@ public class FileConverter {
 			}
 		}
 	}
-	
+
 	public static void writeFile(int toWrite, String filename) throws IOException{
 		try(PrintWriter pw = new PrintWriter(filename)) {
 			pw.println(toWrite);
